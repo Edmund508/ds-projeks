9aca852fff3243b4a9b85c2e6c1041fe1647e6f4
Edmunds Steļmačenoks <edmunds.stelmacenoks@edu.rtu.lv>
2025-05-20 17:23:19 +0000

    v1

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+baselink = "https://www.sudzibas.lv"
+usrinput = input("Ievadiet kompanijas nosaukumu: ")
+
+categorylink = "/category/atputa-izklaide/1"
+page = requests.get(baselink + categorylink)
+if page.status_code == 200:
+    pageCode = BeautifulSoup(page.content, "html.parser")
+    next = pageCode.find_all('a', string = " nākamā")
+    while(next != []):
+        found = pageCode.find_all('b', string = usrinput)
+        for i in found:
+            parent = i.find_parent('a')
+            href = parent.get('href')
+            new_url = baselink + href
+            sid = href.split("-")
+            nextpage = requests.get(new_url)
+            if nextpage.status_code == 200:
+                next_page_code = BeautifulSoup(nextpage.content, "html.parser")
+                name = sid[len(sid) - 1] + ".txt"
+                file = open(name, "w")
+                theme = next_page_code.find_all(itemprop = "about")
+                for t in theme:
+                    file.write(t.text + "\n" + "\n")
+                text = next_page_code.find_all(itemprop = "reviewBody")
+                for t in text:
+                    file.write(t.text + "\n" + "\n")
+                date = next_page_code.find_all(itemprop = "datePublished")
+                for t in date:
+                    file.write(t.text)
+        href = next[0].get('href')
+        page = requests.get(baselink + categorylink + href)
+        if page.status_code == 200:
+            pageCode = BeautifulSoup(page.content, "html.parser")
+            next = pageCode.find_all('a', string = " nākamā")
+
 
